463d0c43969ce2f83252685df7473ab89bc7f2f1
Rio Prastiawan <akunviprio@gmail.com>
2019-09-17 17:00:57 +0700

    create roles & permission system

--- app/Permissions/HasPermissionsTrait.php
+++ app/Permissions/HasPermissionsTrait.php
@@ -7,6 +7,39 @@ use App\Models\Role;
 
 trait HasPermissionsTrait
 {
+
+    public function hasPermissionTo($permission)
+    {
+        return $this->hasPermissionThroughRole($permission) || $this->hasPermission($permission);
+    }
+
+    protected function hasPermissionThroughRole($permission)
+    {
+        foreach($permission->roles as $role) {
+            if($this->roles->contains($role)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function hasPermission($permission)
+    {
+        return (bool) $this->permissions->where('name', $permission->name)->count();
+    }
+
+    public function hasRole(...$roles)
+    {
+        foreach($roles as $role){
+            if($this->roles->contains('name', $role)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public function roles()
     {
         return $this->belongsToMany(Role::class, 'users_roles');
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('admin.index');
+Route::get('/', function (Request $request) {
+    // return view('admin.index');
+    $user = $request->user();
+
+    dd($user->can('add products'));
 });
 
 Auth::routes();

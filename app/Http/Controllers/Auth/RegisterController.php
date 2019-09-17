f31732bc25562a67b45b19662cc57917f0371d27
Rio Prastiawan <akunviprio@gmail.com>
2019-09-17 15:55:41 +0700

    konfigurasi main layout admin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -67,6 +67,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'address' => $data['address'],
+            'phone' => $data['phone'],
         ]);
     }
 }
---
+++ resources/views/admin/templates/default.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    @include('admin.templates.partials._head')
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  @include('admin.templates.partials._header')
+
+  @include('admin.templates.partials._sidebar')
+
+  <div class="content-wrapper">
+    @yield('content')
+  </div>
+
+  @include('admin.templates.partials._footer')
+
+  @include('admin.templates.partials._control-sidebar')
+
+  <div class="control-sidebar-bg"></div>
+</div>
+
+@include('admin.templates.partials._scripts')
+</body>
+</html>
